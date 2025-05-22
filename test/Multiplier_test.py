76827616dcaada731e66a69e8112a19aa660d018
js2992 <js2992@cornell.edu>
2025-05-22 12:56:09 -0400

    2025-05-22

---
+++ test/Multiplier_test.py
@@ -0,0 +1,22 @@
+from TestUtil import *
+
+#==========================================================
+
+async def check(dut, in0, in1, out):
+  dut.in0.value = in0.get()
+  dut.in1.value = in1.get()
+
+  await Timer(1, units="ns")
+  
+  assert (dut.out.value == out.get())
+
+#==========================================================
+
+@cocotb.test()
+async def test_case_1_random(dut):
+  trials = 10000
+  for t in range(trials):
+    in0 = rand_fp(NBITS, DBITS)
+    in1 = rand_fp(NBITS, DBITS)
+    out = mul_fp(in0, in1)
+    await check(dut, in0, in1, out)
